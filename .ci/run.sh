102012af80fb373037da473bfa3f3a609e92deef
Jan Kotanski <jankotan@gmail.com>
2026-05-11 11:34:31 +0200

    add ubuntu 26.04 tests

--- .ci/run.sh
+++ .ci/run.sh
@@ -7,7 +7,12 @@ else
     echo "run nxstaurusgui3"
     docker exec  ndts python3 test
 fi
-if [ "$?" -ne "0" ]
-then
-    exit -1
+ERR=$?
+
+echo "ERROR: "$ERR
+
+if [ $ERR != 0 ]; then
+    if [ $ERR != 139 ]; then
+	exit $ERR;
+    fi
 fi
